fcbede40bdf8522a002d77304b76fc0ae07d73db
kuissoft <fivewisdomzero@gmail.com>
2017-07-30 14:35:24 -0400

    convert functional component to class one

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <h1>Hello from react</h1>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello from react</h1>
+    );
+  }
 }
+
+export default App;
