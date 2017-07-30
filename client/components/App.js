f0a1e18efb356ee56e60923ce217edb89b6a41a5
kuissoft <fivewisdomzero@gmail.com>
2017-07-30 14:37:43 -0400

    add greeting component

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Greetings from './Greetings';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello from react</h1>
+      <Greetings />
     );
   }
 }
