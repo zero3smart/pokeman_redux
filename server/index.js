c6d07b553dd5cb66f703e33b621347432a7dca33
kuissoft <fivewisdomzero@gmail.com>
2017-07-30 14:28:48 -0400

    add webpack

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
+
 let app = express();
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtools: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: '/',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        exclude: /(node_modules|bower_components)/,
+        loaders: [ 'react-hot', 'babel' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '', '.js']
+  }
+}
