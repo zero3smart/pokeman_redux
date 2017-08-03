ea9d73ca9b3009e2ab0eda73cffed09fa68881bc
kuissoft <fivewisdomzero@gmail.com>
2017-08-03 03:13:09 -0400

    sends data to server via redux thunk action

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import timezones from '../../data/timezones';
 import map from 'lodash/map';
-import axios from 'axios';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -24,7 +23,7 @@ class SignupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    axios.post('/api/users', { user: this.state });
+    this.props.userSignupRequest(this.state);
   }
 
   render() {
@@ -95,6 +94,10 @@ class SignupForm extends React.Component {
       </form>
     );
   }
+}
+
+SignupForm.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired
 }
 
 export default SignupForm;
