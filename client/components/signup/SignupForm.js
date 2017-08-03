0745932a5c20a94e92be529584b96203aca7e719
kuissoft <fivewisdomzero@gmail.com>
2017-08-03 02:49:59 -0400

    implement redux

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import timezones from '../../data/timezones';
 import map from 'lodash/map';
+import axios from 'axios';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -23,7 +24,7 @@ class SignupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    console.log(this.state);
+    axios.post('/api/users', { user: this.state });
   }
 
   render() {
